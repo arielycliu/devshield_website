ad177038695e85b4331e14212a65c59b8bcd18c5
Mufenas Muneer <46621819+MufnasMuneer@users.noreply.github.com>
2025-01-29 12:19:48 -0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,41 @@
-import './App.css';
-import LandingPage from './LandingPage';
-function App() {
+// import './App.css';
+// import LandingPage from './LandingPage';
+// function App() {
 
-  return (
-    <>
-      <LandingPage />
-    </>
-  )
+//   return (
+//     <>
+//       <LandingPage />
+//     </>
+//   )
+// }
+
+// export default App
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import LandingPage from './LandingPage';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import TermsAndConditions from './components/TermsAndConditions';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import getLPTheme from './getLPTheme';
+import './App.css';
+
+function App() {
+    const [mode, setMode] = React.useState('dark');
+    const theme = createTheme(getLPTheme(mode));
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                    <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
